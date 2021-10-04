d5e7054eee417440669c1bc125628f8373e20465
Rafael Ghidini <ghidini2209@gmail.com>
2021-10-04 12:24:20 +0100

    project completion

--- src/main/java/com/example/demo/controller/ListController.java
+++ src/main/java/com/example/demo/controller/ListController.java
@@ -2,13 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.service.ListService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/list")
@@ -20,10 +22,14 @@ public class ListController {
 
     @Operation(
             summary = "Sort List",
-            description = "Sort a list of integers in ascending order"
+            description = "Sort a list of integers in ascending order",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ok"),
+                    @ApiResponse(responseCode = "400", description = "Bad request")
+            }
     )
     @PostMapping(path = "/sort", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Integer>> recuperarTodosPaginado(@RequestBody List<Integer> listToBeSorted) {
+    public ResponseEntity<Integer[]> recuperarTodosPaginado(@RequestBody Integer[] listToBeSorted) {
         return ResponseEntity.ok(listService.sortList(listToBeSorted));
     }
 }
